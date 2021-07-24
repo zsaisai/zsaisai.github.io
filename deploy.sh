f291cc790bab8fc5eb7b10bdea18edde74f9c862
SS1997-code <z_ss101@163.com>
2021-07-24 23:26:17 +0800

    update blog: Post at 2021-07-24 23:26:13

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,13 @@
 dt="Post at "`date "+%Y-%m-%d %H:%M:%S"`
 
 hexo clean
+git init
 
 git add .
 
-git commit -am "$dt"
+git commit -am "update blog: $dt"
+
+git remote add origin git@github.com:zsaisai/zsaisai.github.io.git
 
 git push -f -u origin master
 
